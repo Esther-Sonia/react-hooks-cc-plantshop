43e998e7ef2554b650c73e5f6352cc3a20e4f021
Esther-Sonia <esthersonia21@gmail.com>
2025-04-18 08:33:46 +0300

    update plantcard to display plant info & toggle stock status

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const { name, price, image } = plant;
+  const [isSoldOut, setIsSoldOut] = useState(false);
+
+  function handleClick() {
+    setIsSoldOut((prev) => !prev);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {isSoldOut ? (
+        <button onClick={handleClick}>Out of Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button className="primary" onClick={handleClick}>
+          In Stock
+        </button>
       )}
     </li>
   );
