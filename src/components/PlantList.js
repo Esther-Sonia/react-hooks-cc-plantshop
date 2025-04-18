5161877afb169950d599704ac7e27f558c9a9bb8
Esther-Sonia <esthersonia21@gmail.com>
2025-04-18 14:52:06 +0300

    update changes for delete and update functionality

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList({ plants }) {
+function PlantList({ plants,onUpdatePlant, onDeletePlant }) {
   return (
     <ul className="cards">
       {plants.map((plant) => (
-        <PlantCard key={plant.id} plant={plant} />
+        <PlantCard key={plant.id} plant={plant} onUpdatePlant={onUpdatePlant}
+        onDeletePlant={onDeletePlant} />
       ))}
     </ul>
   );
